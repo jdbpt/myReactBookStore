9340b50ca33a3b71e9707b22c34df385f1d0bd7e
jdbpt <jbrown5.filn@gmail.com>
2023-07-21 10:28:18 -0400

    additional styling

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,7 @@
 import Header from "../components/Header";
 function Home(){
     return(
-        <div>
+        <div className="homeBkrd">
             <Header/>
             <div className="home extraMargin">
             
